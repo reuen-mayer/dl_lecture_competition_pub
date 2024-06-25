9f4bea7bd5f4765f87ee13df474f53e73d0b0d1c
reuen-mayer <scarletnolight@gmail.com>
2024-06-26 00:57:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -405,5 +405,5 @@ def main():
     torch.save(model.state_dict(), "model.pth")
     np.save("drive/MyDrive/Colab Notebooks/DL2024S/Final/submission.npy", submission)
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
